f99a463c01e5f0ee00997d25a337cb0e53f70160
a-sowa <axelsowapro@gmail.com>
2024-06-28 13:14:00 +0200

    Handling display while user connected

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Routes from './components/Routes';
+import { UidContext } from "./components/AppContext";
+import axios from "axios";
 
 const App = () => {
+    const [uid, setUid] = useState(null);
+
+    useEffect(() => {
+        const fetchToken = async() => {
+            await axios({
+                method: "get",
+                url: `${process.env.REACT_APP_API_URL}jwtid`,
+                withCredentials: true
+            })
+            .then((res) => {
+                setUid(res.data);
+            })
+            .catch((err) => console.log("No token"))
+        }
+        fetchToken();
+    }, [uid]);
+
     return (
-        <div>
+        <UidContext.Provider value={uid}>
             <Routes />
-        </div>
+        </UidContext.Provider>
     );
 };
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
