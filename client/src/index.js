99daeac2c750ef7beaf7c3e4edabcf0c6ce574fe
a-sowa <axelsowapro@gmail.com>
2024-06-28 17:15:03 +0200

    Setting Redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App'
+import App from './App';
 import './styles/index.css';
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import {composeWithDevTools} from '@redux-devtools/extension';
+import logger from 'redux-logger';
+import {thunk} from 'redux-thunk';
+import rootReducer from './reducers'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = createStore(
+    rootReducer, composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
 root.render(
-    <App />
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
