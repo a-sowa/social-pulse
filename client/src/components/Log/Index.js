14298a7b6b2707caca76d94ac62deae42afa8b2d
a-sowa <axelsowapro@gmail.com>
2024-06-28 12:46:10 +0200

    Creating Log component

--- client/src/components/Log/Index.js
+++ client/src/components/Log/Index.js
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SignUpForm from './SignUpForm';
+import SignInForm from './SignInForm';
 
-const Index = () => {
+const Log = (props) => {
+    const [signUpModal, setSignUpModal] = useState(props.signup);
+    const [signInModal, setSignInModal] = useState(props.signin);
+
+    const handleModals = (e) => {
+        if (e.target.id === "register") {
+            setSignInModal(false);
+            setSignUpModal(true);
+        } else if (e.target.id === "login") {
+            setSignUpModal(false);
+            setSignInModal(true);
+        }
+    }
+
   return (
-    <div>Log</div>
+    <div className="connection-form">
+        <div className="form-container">
+            <ul>
+                <li 
+                    onClick={handleModals} 
+                    id='register' 
+                    className={signUpModal ? "active-btn" : null}
+                >
+                    S'inscrire
+                </li>
+                <li 
+                    onClick={handleModals} 
+                    id='login'
+                    className={signInModal ? "active-btn" : null}
+                >
+                    Se connecter
+                </li>
+            </ul>
+            {signUpModal && <SignUpForm />}
+            {signInModal && <SignInForm />}
+        </div>
+    </div>
   )
 }
 
-export default Index
+export default Log
