eee55b2377e8040dee9d9d7c9daaccb756607d63
a-sowa <axelsowapro@gmail.com>
2024-07-01 22:37:45 +0200

    Handling likes and nlikes

--- client/src/actions/post.action.js
+++ client/src/actions/post.action.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const GET_POSTS = "GET_POSTS";
+export const LIKE_POST = "LIKE_POST";
+export const UNLIKE_POST = "UNLIKE_POST";
 
 export const getPosts = () => {
   return (dispatch) => {
@@ -12,3 +14,32 @@ export const getPosts = () => {
       .catch((err) => console.log(err));
   };
 };
+
+export const likePost = (postId, userId) => {
+  return (dispatch) => {
+    return axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}api/post/like-post/` + postId,
+      data: { id: userId}
+    })
+    .then((res) => {
+      dispatch({ type: LIKE_POST, payload: { postId, userId }});
+    })
+    .catch((err) => console.log(err));
+  }
+}
+
+export const unlikePost = (postId, userId) => {
+  return (dispatch) => {
+    return axios({
+      method: "patch",
+      url: `${process.env.REACT_APP_API_URL}api/post/unlike-post/` + postId,
+      data: { id: userId}
+    })
+    .then((res) => {
+      dispatch({ type: UNLIKE_POST, payload: { postId, userId }});
+    })
+    .catch((err) => console.log(err));
+  }
+}
+
