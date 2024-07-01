848baabe8d62fe326062a065999736552a268d8d
a-sowa <axelsowapro@gmail.com>
2024-07-01 10:17:53 +0200

    Creating Profil page and update profile picture

--- client/src/pages/Profil.js
+++ client/src/pages/Profil.js
@@ -1,22 +1,23 @@
 import React, { useContext } from "react";
-import Log from '../components/Log/Index'
+import Log from "../components/Log/Index";
 import { UidContext } from "../components/AppContext";
+import UpdateProfil from "../components/Profil/UpdateProfil";
 
 const Profil = () => {
-    const uid = useContext(UidContext);
-    return (
-        <div className="profil-page">
-            {uid ? (
-                <h1>UPDATE PAGE</h1>
-            ) : (
-                <div className="log-container">
-                    <Log signin={false} signup={true}/>
-                    <div className="img-container">
-                        <img src="./img/log.svg" alt="img-log" />
-                    </div>
-                </div>
-            )}
+  const uid = useContext(UidContext);
+  return (
+    <div className="profil-page">
+      {uid ? (
+        <UpdateProfil />
+      ) : (
+        <div className="log-container">
+          <Log signin={false} signup={true} />
+          <div className="img-container">
+            <img src="./img/log.svg" alt="img-log" />
+          </div>
         </div>
+      )}
+    </div>
   );
 };
 
