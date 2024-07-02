d9b85396691a74c8910864795581d9beb91321fb
a-sowa <axelsowapro@gmail.com>
2024-07-02 11:53:56 +0200

    Handling frontend CRUD for posts'

--- client/src/components/Post/Card.js
+++ client/src/components/Post/Card.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { dateParser, isEmpty } from "../Utils";
 import FollowHandler from "../Profil/FollowHandler";
 import LikeButton from "./LikeButton";
+import { updatePost } from "../../actions/post.action";
+import DeleteCard from "./DeleteCard";
 
 const Card = ({ post }) => {
   const [isLoading, setIsLoading] = useState(true);
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [textUpdate, setTextUpdate] = useState(null);
   const usersData = useSelector((state) => state.usersReducer);
   const userData = useSelector((state) => state.userReducer);
+  const dispatch = useDispatch();
+
+  const updateItem = () => {
+    if (textUpdate) {
+      dispatch(updatePost(post._id, textUpdate))
+    }
+    setIsUpdated(false);
+  }
 
   useEffect(() => {
     !isEmpty(usersData[0]) && setIsLoading(false);
@@ -26,10 +38,11 @@ const Card = ({ post }) => {
                 usersData
                   .map((user) => {
                     if (user._id === post.posterId) return user.picture;
+                    else return null;
                   })
                   .join("")
               }
-              alt="Photo de profil"
+              alt="Profil"
             />
           </div>
           <div className="card-right">
@@ -40,6 +53,7 @@ const Card = ({ post }) => {
                     usersData
                       .map((user) => {
                         if (user._id === post.posterId) return user.pseudo;
+                        else return null;
                       })
                       .join("")}
                 </h3>
@@ -49,20 +63,44 @@ const Card = ({ post }) => {
               </div>
               <span>{dateParser(post.createdAt)}</span>
             </div>
-            <p>{post.message}</p>
+
+            {isUpdated === false && <p>{post.message}</p>}
+            {isUpdated && (
+              <div className="update-post">
+                <textarea
+                  defaultValue={post.message}
+                  onChange={(e) => setTextUpdate(e.target.value)}
+                />
+                <div className="button-container">
+                  <button className="btn" onClick={updateItem}>
+                    Valider les modifications
+                  </button>
+                </div>
+              </div>
+            )}
+
             {post.picture && (
               <img src={post.picture} alt="card-pic" className="card-pic" />
             )}
             {post.video && (
               <iframe
-              width="500"
-              height="300"
-              src={post.video}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title={post._id}
-            ></iframe>
+                width="500"
+                height="300"
+                src={post.video}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title={post._id}
+              ></iframe>
+            )}
+
+            {userData._id === post.posterId && (
+              <div className="button-container">
+                <div onClick={() => setIsUpdated(!isUpdated)}>
+                  <img src="./img/icons/edit.svg" alt="edit" />
+                </div>
+                <DeleteCard id={post._id} />
+              </div>
             )}
             <div className="card-footer">
               <div className="comment-icon">
