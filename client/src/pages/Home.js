3fc0af4c365d4782e3db53cdcce481e6c09d6345
a-sowa <axelsowapro@gmail.com>
2024-07-02 16:00:52 +0200

    Creating post form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import LeftNav from "../components/LeftNav.js";
 import Thread from "../components/Thread.js";
-
+import { UidContext } from "../components/AppContext.js";
+import NewPostForm from "../components/Post/NewPostForm.js";
+import Log from "../components/Log/Index.js";
 
 const Home = () => {
-    return (
-        <div className="home">
-            <LeftNav />
-            <div className="main">
-                <Thread />
-            </div>
+  const uid = useContext(UidContext);
+  return (
+    <div className="home">
+      <LeftNav />
+      <div className="main">
+        <div className="home-header">
+        {uid ? <NewPostForm /> : <Log signin={true} signup={false} />}
         </div>
-    );
+        <Thread />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
